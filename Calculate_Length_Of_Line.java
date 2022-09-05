18bf223feaed2e6d1bf2af5d01670c2afd3c7886
sunilmsd007 <sunilmsd007@gmail.com>
2022-09-05 23:33:47 +0530

    comparison of length of 2 lines

--- Calculate_Length_Of_Line.java
+++ Calculate_Length_Of_Line.java
@@ -13,7 +13,25 @@ public class Calculate_LengthOf_Line {
 		int x2 = sc.nextInt();
 		System.out.println("enter Y2 co-ordinate");
 		int y2 = sc.nextInt();
-		double length = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-		System.out.println("length of the line is: " + length);
+		double length1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		System.out.println("length of the line1 is: " + length1);
+
+                System.out.println("enter second line co-ordinates");
+                System.out.println("enter A1 co-ordinate");
+		int a1 = sc.nextInt();
+		System.out.println("enter B1 co-ordinate");
+		int b1 = sc.nextInt();
+		System.out.println("enter A2 co-ordinate");
+		int a2 = sc.nextInt();
+		System.out.println("enter B2 co-ordinate");
+		int b2 = sc.nextInt();
+		double length2 = Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
+		System.out.println("length of line2 is: "+length2);
+		String str1 = Double.toString(length1);
+    	        String str2 = Double.toString(length2);
+		if(str1.equals(str2))
+			System.out.println("both lines are equal");
+		else
+			System.out.println("both lines are not equal");
 	}
 }
